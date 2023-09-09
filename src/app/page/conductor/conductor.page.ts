1feced8d0a97681fb8e0066afee2b4e4415e6dc4
SebastianAraneda1 <sebastian_lb18@outlook.cl>
2023-09-09 11:19:00 -0300

    Cambios conductor

--- src/app/page/conductor/conductor.page.ts
+++ src/app/page/conductor/conductor.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-conductor',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConductorPage implements OnInit {
 
-  constructor() { }
+  formViaje = {
+    destino: "",
+    cantidad: "",
+    monto: 0,
+    patente: ""
+  }
+
+  showErrorDestino:boolean = false;
+  showErrorCantidad:boolean = false;
+  showErrorMonto:boolean = false;
+  showErrorPatente:boolean = false;
+
+  constructor(private route:Router) { }
 
   ngOnInit() {
+  }
+
+  viaje() {
+    let destino = false;
+    let cantidad = false;
+    let monto = false;
+    let patente = false;
+
+    const patenteRegexActual = /^[A-Z]{4}\d{2}$/;
+    const patenteRefexAnterior = /^[A-Z]{2}\d{4}$/ 
+
+    if(this.formViaje.destino.length > 3){
+      destino = true;
+    }else{
+      destino = false;
+    }
   }
 
 }
